c6fa4651e800a90a5f03c0fd6d2a14e97ab8106c
Gobbur <sgobbur@8c85904cc8ae.ant.amazon.com>
2019-10-28 16:26:54 -0700

    also checking user terminal activity

--- scripts/auto-stop-idle/autostop.py
+++ scripts/auto-stop-idle/autostop.py
@@ -11,6 +11,7 @@
 #     express or implied. See the License for the specific language governing
 #     permissions and limitations under the License.
 
+import os
 import requests
 from datetime import datetime
 import getopt, sys
@@ -23,7 +24,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Usage
 usageInfo = """Usage:
 This scripts checks if a notebook is idle for X seconds if it does, it'll stop the notebook:
-python autostop.py --time <time_in_seconds> [--port <jupyter_port>] [--ignore-connections]
+python autostop.py --time <time_in_seconds> [--port <jupyter_port>] [--ignore-connections] [--ignore-terminals]
 Type "python autostop.py -h" for available options.
 """
 # Help info
@@ -33,6 +34,8 @@ helpInfo = """-t, --time
     jupyter port
 -c --ignore-connections
     Stop notebook once idle, ignore connected users
+-u --ignore-terminals
+    Ignore terminals
 -h, --help
     Help information
 """
@@ -41,8 +44,9 @@ helpInfo = """-t, --time
 idle = True
 port = '8443'
 ignore_connections = False
+ignore_terminals = False
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "ht:p:c", ["help","time=","port=","ignore-connections"])
+    opts, args = getopt.getopt(sys.argv[1:], "ht:p:cu", ["help","time=","port=","ignore-connections","ignore-terminals"])
     if len(opts) == 0:
         raise getopt.GetoptError("No input parameters!")
     for opt, arg in opts:
@@ -55,6 +59,8 @@ try:
             port = str(arg)
         if opt in ("-c", "--ignore-connections"):
             ignore_connections = True
+        if opt in ("-u", "--ignore-terminals"):
+            ignore_terminals = True
 except getopt.GetoptError:
     print(usageInfo)
     exit(1)
@@ -82,6 +88,18 @@ def get_notebook_name():
         _logs = json.load(logs)
     return _logs['ResourceName']
 
+def get_terminals(dir):
+    terminals = {}
+    for pts in os.listdir(dir):
+        if pts.isdigit():
+            pts_path = dir + '/' + pts
+            mtime = os.path.getmtime(pts_path)
+            terminals[pts_path] = datetime.fromtimestamp(mtime).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return terminals
+
+session_count = 0
+terminal_count = 0
+
 # This is hitting Jupyter's sessions API: https://github.com/jupyter/jupyter/wiki/Jupyter-Notebook-Server-API#Sessions-API
 response = requests.get('https://localhost:'+port+'/api/sessions', verify=False)
 data = response.json()
@@ -89,6 +107,7 @@ if len(data) > 0:
     for notebook in data:
         # Idleness is defined by Jupyter
         # https://github.com/jupyter/notebook/issues/4634
+        session_count += 1
         if notebook['kernel']['execution_state'] == 'idle':
             if not ignore_connections:
                 if notebook['kernel']['connections'] == 0:
@@ -101,7 +120,15 @@ if len(data) > 0:
                     idle = False
         else:
             idle = False
-else:
+
+if not ignore_terminals:
+    terminals = get_terminals('/dev/pts')
+    terminal_count = len(terminals)
+    for pts in terminals:
+        if not is_idle(terminals[pts]):
+            idle = False
+
+if session_count == 0 and terminal_count == 0:
     client = boto3.client('sagemaker')
     uptime = client.describe_notebook_instance(
         NotebookInstanceName=get_notebook_name()
